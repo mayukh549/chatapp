88bc42fb20c25fe53f360292db6b5110a5c4e502
_RICKK_ <mayukh.10.2003@gmail.com>
2024-07-09 15:38:51 +0530

    register page

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -3,34 +3,118 @@ import logo from "D:\\projects\\livechatapp\\client\\src\\icons\\communication.p
 import { Backdrop, Button, CircularProgress, TextField } from "@mui/material";
 
 function Login() {
+  const [showlogin, setShowLogin] = useState(false);
   return (
     <div className='login-container'>
-        <div className='image-container'>
-            <img src={logo} alt="Logo" className="welcome-logo" />
+      <div className='image-container'>
+        <img src={logo} alt="Logo" className="welcome-logo" />
 
+      </div>
+      {showlogin && (<div className='login-box'>
+        <p className="login-text">Login to your Account</p>
+        <TextField
+
+          id="standard-basic"
+          label="Enter Email Address"
+          variant="outlined"
+
+        />
+        <TextField
+
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+
+        />
+        <Button
+          variant="outlined">Login</Button>
+        <p>
+          Don't have an Account ?{" "}
+          <span
+            className="hyper"
+          onClick={() => {
+            setShowLogin(false);
+          }}
+          >
+            Sign Up
+          </span>
+        </p>
         </div>
-        <div className='login-box'>
-            <p className="login-text">Login to your Account</p>
+        )}
+        
+        {!showlogin && (
+          <div className="login-box">
+            <p className="login-text">Create your Account</p>
             <TextField
-
+              // onChange={changeHandler}
+              id="standard-basic"
+              label="Enter User Name"
+              variant="outlined"
+              color="secondary"
+              name="name"
+              helperText=""
+              // onKeyDown={(event) => {
+              //   if (event.code == "Enter") {
+              //     // console.log(event);
+              //     signUpHandler();
+              //   }
+              // }}
+            />
+            <TextField
+              // onChange={changeHandler}
               id="standard-basic"
               label="Enter Email Address"
               variant="outlined"
-
+              color="secondary"
+              name="email"
+              // onKeyDown={(event) => {
+              //   if (event.code == "Enter") {
+              //     // console.log(event);
+              //     signUpHandler();
+              //   }
+              // }}
             />
             <TextField
-
+              // onChange={changeHandler}
               id="outlined-password-input"
               label="Password"
               type="password"
               autoComplete="current-password"
-
+              color="secondary"
+              name="password"
+              // onKeyDown={(event) => {
+              //   if (event.code == "Enter") {
+              //     // console.log(event);
+              //     signUpHandler();
+              //   }
+              // }}
             />
             <Button
-              variant="outlined">Login</Button>
+              variant="outlined"
+              // onClick={signUpHandler}
+            >
+              Sign Up
+            </Button>
+            <p>
+              Already have an Account ?
+              <span
+                className="hyper"
+                onClick={() => {
+                  setShowLogin(true);
+                }}
+              >
+                Log in
+              </span>
+            </p>
+            {/* {signInStatus ? (
+              <Toaster key={signInStatus.key} message={signInStatus.msg} />
+            ) : null} */}
+          </div>
+        )}
 
-        </div>
-        
+      
+
 
     </div>
   )
